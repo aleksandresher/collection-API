f00b7c92176479c93f0850d35f9634ca2d466376
aleksandresher <89658328+aleksandresher@users.noreply.github.com>
2023-07-20 15:27:11 +0400

    Update socket.js

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ module.exports = {
   init: (httpServer) => {
     io = require("socket.io")(httpServer, {
       cors: {
-        origin: "http://localhost:3000",
+        origin: "https://collection-hub.onrender.com",
         credentials: true,
       },
     });
